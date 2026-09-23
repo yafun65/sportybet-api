cf587bc7e503802b4edc37e05f67a6e4905122a5
yafun65 <84651818+yafun65@users.noreply.github.com>
2026-09-23 20:59:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,29 @@ import express from "express";
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://sportybet-slip-optimizer.vercel.app"
+  );
+
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,POST,OPTIONS"
+  );
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 10000;
